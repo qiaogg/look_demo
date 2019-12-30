ee271fc7fd6f095579c0aa19efafdc6e8a1cf933
pachimari <c887811c@163.com>
2019-12-30 16:41:34 +0800

    add word cloud data
    add word cloud data

--- src/components/cloud.vue
+++ src/components/cloud.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-        <div class="title" style="font-size: 23px;padding-left: 20px; margin-bottom: 20px;float:left">
-              <span class="el-icon-search"></span>
-              明星八卦热词
-        </div>
-        <br>
-        <el-card style="margin-top:40px">
-                <div id="word_cloud1"></div>
-        </el-card>
+    <div class="title" style="font-size: 23px;padding-left: 20px; margin-bottom: 20px;float:left">
+      <span class="el-icon-search"></span>
+      明星八卦热词
+    </div>
+    <br>
+    <el-card style="margin-top:40px">
+      <div id="word_cloud1"></div>
+    </el-card>
   </div>
 </template>
 
@@ -15,11 +15,129 @@
   import echarts from 'echarts';
   import 'echarts-wordcloud';
   import data from '../../static/data/data.json';
-  
+
   export default {
     data () {
       return {
-        chart: null
+        data1:[
+          {
+            name:"王思聪妈妈帮还债",
+            value:200000
+          },
+          {
+            name:"佟丽娅晒圣诞美照",
+            value:179837
+          },
+          {
+            name:"王思聪限消令和解",
+            value:96192
+          },
+          {
+            name:"徐子珊退出娱乐圈",
+            value:92531
+          },
+          {
+            name:"陈坤儿子近照",
+            value:87513
+          },
+          {
+            name:"徐锦江的圣诞祝福",
+            value:73308
+          },
+          {
+            name:"刘昊然买液态屁",
+            value:72287
+          },
+          {
+            name:"papi酱孕肚",
+            value:71285
+          },
+          {
+            name:"炎亚纶向路人道歉",
+            value:69674
+          },
+          {
+            name:"杨烁回应教育争议",
+            value:65619
+          },
+          {
+            name:"87红楼梦演员重聚",
+            value:42090
+          },
+          {
+            name:"冯小刚调侃徐帆",
+            value:41530
+          },
+          {
+            name:"靳东王鸥同框",
+            value:41522
+          },
+          {
+            name:"美丽人生定档",
+            value:38083
+          },
+          {
+            name:"刘銮雄太太消费",
+            value:33575
+          },
+          {
+            name:"王思聪被执行和解",
+            value:30566
+          },
+          {
+            name:"泫雅李佳琦同框",
+            value:11887
+          },
+          {
+            name:"曝王思聪被逼联姻",
+            value:10248
+          },
+          {
+            name:"王阳明宣布当爸",
+            value:9985
+          },
+          {
+            name:"魏大勋肚皮舞",
+            value:9500
+          },
+          {
+            name:"老友记确定重聚",
+            value:8355
+          },
+          {
+            name:"魔兽世界怀旧服",
+            value:7700
+          },
+          {
+            name:"易烊千玺演唱会",
+            value:6945
+          },
+          {
+            name:"刘鑫微博被封",
+            value:6418
+          },
+          {
+            name:"林宗虎逝世",
+            value:5423
+          },
+          {
+            name:"张瀞仁离世",
+            value:5345
+          },
+          {
+            name:"易烊千玺参加军训",
+            value:4549
+          },
+          {
+            name:"唐嫣圣诞节自拍",
+            value:4165
+          },
+          {
+            name:"生化危机2重制版",
+            value:2041
+          },
+
+        ]
       };
     },
     methods: {
@@ -34,6 +152,7 @@
             right: 20,
             top: 20
           },
+          tooltip:{},
           series: [{
             type: 'wordCloud',
             shape: myshape,
@@ -46,10 +165,11 @@
             height: '80%',
             right: null,
             bottom: null,
-            sizeRange: [12, 75],
-            rotationRange: [-90, 90],
+            sizeRange: [10, 80],
+            rotationRange: [0, 0],
             rotationStep: 45,
-            gridSize: 8,
+            gridSize: 15,
+
             textStyle: {
               normal: {
                 fontFamily: 'Microsoft Yahei',
@@ -67,7 +187,7 @@
                 shadowColor: '#333'
               }
             },
-            data: data.cloud.wordContent
+            data: this.data1,
           }]
         });
       }
@@ -75,7 +195,7 @@
     mounted() {
       // 保证this.$el已经插入文档
       this.$nextTick(function() {
-        this.drawCloud('word_cloud1', 'cardioid');
+        this.drawCloud('word_cloud1', 'cardioid');//cardioid
         this.drawCloud('word_cloud2', 'pentagon');
         var that = this;
         var resizeTimer = null;
